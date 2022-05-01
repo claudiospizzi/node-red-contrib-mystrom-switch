fc644a8d1dd5643a38ca09a697dc91679e55e7a7
stilicrafter <stilicrafter@gmail.com>
2022-05-01 21:09:55 +0200

    Added avgWattSeconds to MyStromSwitchStatus.ts

--- src/modules/MyStromSwitchStatus.ts
+++ src/modules/MyStromSwitchStatus.ts
@@ -5,4 +5,5 @@ export class MyStromSwitchStatus {
   public power: number | undefined;
   public relay: boolean | undefined;
   public temperature: number | undefined;
+  public avgWattSeconds: number | undefined;
 }
